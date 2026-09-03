18d9ca72007d651c418c6f963ac5ee65a30c705e
tarunjethwaniHW <TarunJ@hexaware.com>
2026-09-03 10:21:10 +0530

    Add files via upload

---
+++ lendwise-passthrough-service/src/main/java/com/lendwise/passthrough/LendWisePassthroughApplication.java
@@ -0,0 +1,12 @@
+package com.lendwise.passthrough;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendWisePassthroughApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LendWisePassthroughApplication.class, args);
+    }
+}
